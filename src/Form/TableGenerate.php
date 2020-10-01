34a4a3422823ba9a9342e4ded21cdebe7fe27232
Veoz <VEoz.id@gmail.com>
2020-10-01 13:23:50 +0300

    all 'add' buttons is works!

--- src/Form/TableGenerate.php
+++ src/Form/TableGenerate.php
@@ -16,29 +16,21 @@ class TableGenerate extends FormBase {
     
     $form['#prefix'] = "<div id='test_test'>";
     $form['#suffix'] = "</div>";
-    $table_count = 1;
-    $last_table_name = 100500;
-    if ($form_state->getTriggeringElement()['#name'] == "add_table") {
-      $table_count = $form_state->getTriggeringElement()['#table_count'] + 1;
-      $last_table_name = $form_state->getTriggeringElement(
-        )['#last_table_name'] + 1;
+    
+    if ($form_state->get('last_table_name')) {
+      if ($form_state->getTriggeringElement()['#name'] == "add_table") {
+        $last_table_name = $form_state->getTriggeringElement(
+          )['#last_table_name'] + 1;
+        $form_state->set("last_table_name", $last_table_name);
+      }
+    } else {
+      $form_state->set("last_table_name", 100500);
     }
-    $form['actions-bot']['button'] = [
-      '#type' => 'button',
-      '#name' => 'add_table',
-      '#value' => 'Add table',
-      '#table_count' => $table_count,
-      '#last_table_name' => $last_table_name,
-      '#attributes' => ['class' => ["test-test"]],
-      '#ajax' => [
-        'callback' => '::ajaxSubmitCallback',
-        'wrapper' => 'test_test',
-      ],
-    ];
-    $table_count = $form['actions-bot']['button']['#table_count'];
+    
+    
     if ($form_state->getValue('100500')) {
       $order_list = [];
-      for ($n = 100500; $n <= $last_table_name; $n++) {
+      for ($n = 100500; $n <= $form_state->get('last_table_name'); $n++) {
         $test = $form_state->getValue($n);
         $form_current_fields = count($test);
         array_push($order_list, $form_current_fields);
@@ -48,7 +40,18 @@ class TableGenerate extends FormBase {
       $order_list[] = 1;
     }
     $form_state->set('order_list', $order_list);
-    $this->tableSkeleton($form, $form_state, $table_count);
+    $form = $this->tableSkeleton($form, $form_state);
+    $form['actions-bot']['button'] = [
+      '#type' => 'button',
+      '#name' => 'add_table',
+      '#value' => 'Add table',
+      '#last_table_name' => $form_state->get('last_table_name'),
+      '#attributes' => ['class' => ["test-test"]],
+      '#ajax' => [
+        'callback' => '::ajaxSubmitCallback',
+        'wrapper' => 'test_test',
+      ],
+    ];
     return $form;
   }
   
@@ -59,22 +62,23 @@ class TableGenerate extends FormBase {
   }
   
   
-  public function tableSkeleton(&$form, $form_state, $table_count) {
+  public function tableSkeleton($form, $form_state) {
     $first_table = 100500;
 //    $new_year = 1;
     $orders = $form_state->get('order_list');
     $i = 0;
     foreach ($orders as $table_set => $field_set) {
-      if ($field_set == FALSE) $field_set++;
+      if ($field_set == FALSE) $field_set ++;
       $new_year = $field_set; //!!!
       $table_name = $first_table + $i;
       $actions_name = 'actions_' . $table_name;
-      if ($form_state->getTriggeringElement()['#name'] == "add_year") {
-        $new_year = $form_state->getTriggeringElement()['#add_field'] + 1; //!!!
+      if ($form_state->getTriggeringElement()['#name'] == "add_year{$i}") {
+        $new_year = $form_state->getTriggeringElement()['#add_field'] + 1;//!!!
       }
+      
       $form[$actions_name]['button'] = [
         '#type' => 'button',
-        '#name' => 'add_year',
+        '#name' => 'add_year' . $i,
         '#value' => 'Add year',
         '#add_field' => $new_year, //!!!
         '#ajax' => [
@@ -83,7 +87,6 @@ class TableGenerate extends FormBase {
         ],
       ];
       $add_field = $form[$actions_name]['button']['#add_field'];
-  
       $form[$table_name] = [
         '#type' => 'table',
         '#header' => [
@@ -108,80 +111,71 @@ class TableGenerate extends FormBase {
         ],
       ];
       
-      // start row loop
-      $form = $this->tableRow(
-        $form,
-        $form_state,
-        $table_name,
-        $add_field
-      );
+    
       
-      $i++;
-    }
-  }
-  
-  public function tableRow(&$form, $form_state, $table_name, $add_field) {
-    $year = date('Y');
-    $orders = $form_state->get('order_list');
-    for ($coll = 1; $coll <= $add_field; $coll++) {
-      $index = $coll - 1;
-      $form[$table_name][$index]['year'] = [
-        '#type' => 'textfield',
-        '#required' => FALSE,
-        '#value' => $year,
-        '#attributes' => [
-          'readonly' => 'readonly',
-        ],
-      ];
-      $year--;
-      $seasons = [
-        'jan',
-        'feb',
-        'mar',
-        'Q1',
-        'apr',
-        'may',
-        'jun',
-        'Q2',
-        'jul',
-        'aug',
-        'sep',
-        'Q3',
-        'oct',
-        'nov',
-        'dec',
-        'Q4',
-        'YTD',
-      ];
-      foreach ($seasons as $months) {
-        $isQ = 'Q';
-        $isYTD = 'YTD';
-        if (stristr($months, $isQ) != FALSE || stristr(
-            $months,
-            $isYTD
-          ) == 'YTD') {
-          $form[$table_name][$index][$months] = [
-            '#type' => 'textfield',
-            '#required' => FALSE,
-            '#value' => $months,
-            '#name' => $months,
-            '#attributes' => [
-              'readonly' => 'readonly',
-            ],
-          ];
-        }
-        else {
-          $form[$table_name][$index][$months] = [
-            '#type' => 'number',
-            '#required' => FALSE,
-            '#name' => $months,
-          ];
+      $year = date('Y');
+      
+      for ($coll = 1; $coll <= $add_field; $coll++) {
+        $index = $coll - 1;
+        $form[$table_name][$index]['year'] = [
+          '#type' => 'textfield',
+          '#required' => FALSE,
+          '#value' => $year,
+          '#attributes' => [
+            'readonly' => 'readonly',
+          ],
+        ];
+        $year--;
+        $seasons = [
+          'jan',
+          'feb',
+          'mar',
+          'Q1',
+          'apr',
+          'may',
+          'jun',
+          'Q2',
+          'jul',
+          'aug',
+          'sep',
+          'Q3',
+          'oct',
+          'nov',
+          'dec',
+          'Q4',
+          'YTD',
+        ];
+        foreach ($seasons as $months) {
+          $isQ = 'Q';
+          $isYTD = 'YTD';
+          if (stristr($months, $isQ) != FALSE || stristr(
+              $months,
+              $isYTD
+            ) == 'YTD') {
+            $form[$table_name][$index][$months] = [
+              '#type' => 'textfield',
+              '#required' => FALSE,
+              '#value' => $months,
+              '#name' => $months,
+              '#attributes' => [
+                'readonly' => 'readonly',
+              ],
+            ];
+          }
+          else {
+            $form[$table_name][$index][$months] = [
+              '#type' => 'number',
+              '#required' => FALSE,
+              '#name' => $months,
+            ];
+          }
         }
       }
+      $i++;
     }
+    
     return $form;
   }
-  
   public function ajaxSubmitCallback(
     array &$form,
     FormStateInterface $form_state
